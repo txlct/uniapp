4f2df739878cca841c002b7850e5843b2ae8830e
whatif <>
2022-08-17 19:42:08 +0800

    feat: remove shadow css

--- packages/uni-h5/lib/h5/uni.config.js
+++ packages/uni-h5/lib/h5/uni.config.js
@@ -1,24 +1,19 @@
 const fs = require('fs')
 const path = require('path')
 
-function getTemplatePath(template) {
+function getTemplatePath (template) {
   if (template) {
     const userTemplate = path.resolve(process.env.UNI_INPUT_DIR, template)
-    if (fs.existsSync(userTemplate))
-      return userTemplate
+    if (fs.existsSync(userTemplate)) { return userTemplate }
   }
   return path.resolve(process.env.UNI_CLI_CONTEXT, 'public/index.html')
 }
 
-function transform(content) {
-  if (process.env.NODE_ENV === 'production') {
-    return content + // shadow
-      `body::after{position:fixed;content:'';left:-1000px;top:-1000px;-webkit-animation:shadow-preload .1s;-webkit-animation-delay:3s;animation:shadow-preload .1s;animation-delay:3s}@-webkit-keyframes shadow-preload{0%{background-image:url(https://cdn.dcloud.net.cn/img/shadow-grey.png)}100%{background-image:url(https://cdn.dcloud.net.cn/img/shadow-grey.png)}}@keyframes shadow-preload{0%{background-image:url(https://cdn.dcloud.net.cn/img/shadow-grey.png)}100%{background-image:url(https://cdn.dcloud.net.cn/img/shadow-grey.png)}}`
-  }
+function transform (content) {
   return content
 }
 
-function getIndexCssPath(assetsDir, template) {
+function getIndexCssPath (assetsDir, template) {
   const VUE_APP_INDEX_CSS_HASH = process.env.VUE_APP_INDEX_CSS_HASH
   if (VUE_APP_INDEX_CSS_HASH) {
     try {
@@ -39,13 +34,13 @@ module.exports = {
     filterTag: 'wxs',
     vue: '@dcloudio/vue-cli-plugin-uni/packages/h5-vue'
   },
-  copyWebpackOptions(platformOptions, vueOptions) {
+  copyWebpackOptions (platformOptions, vueOptions) {
     const copyOptions = [{
-        from: require.resolve('@dcloudio/uni-h5/dist/index.css'),
-        to: getIndexCssPath(vueOptions.assetsDir, platformOptions.template),
-        transform
-      },
-      'hybrid/html'
+      from: require.resolve('@dcloudio/uni-h5/dist/index.css'),
+      to: getIndexCssPath(vueOptions.assetsDir, platformOptions.template),
+      transform
+    },
+    'hybrid/html'
     ]
     global.uniModules.forEach(module => {
       copyOptions.push('uni_modules/' + module + '/hybrid/html')
